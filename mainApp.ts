7f86c1fc831fdc5bddb5dcf515fd3fed8b341d2c
Okwudili-Daniel <144442872+Okwudili-Daniel@users.noreply.github.com>
2023-11-24 13:39:41 +0100

    Daniel

--- mainApp.ts
+++ mainApp.ts
@@ -1,7 +1,9 @@
 import { Application, Request, Response } from "express";
+import task from "./ROUTER/taskRouter"
 
 const mainApp = (app: Application) =>{
     try {
+        app.use("api/v1", task)
         app.get("/", (req: Request, res: Response) =>{
             try {
                 return res.status(200).json({
