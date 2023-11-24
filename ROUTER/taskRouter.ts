2c5bcd22d1bc24ddeefcb0c0e98afa291dc1c458
JoanJeca <joannajeca936@gmail.com>
2023-11-24 13:46:22 +0100

    Routers

--- ROUTER/taskRouter.ts
+++ ROUTER/taskRouter.ts
@@ -1,10 +1,11 @@
 import {Router} from 'express'
-import { createTask, viewTask } from '../controller/taskController'
+import { createTask, viewOneTask, viewTask } from '../controller/taskController'
 
 const router:Router = Router()
 
 router.route("/create-task").post(createTask)
 router.route("/view-task").get(viewTask)
-router.route("/update-task")
+router.route("/view-one-task").get(viewOneTask)
+router.route("/update-task") 
 
 export default router 
