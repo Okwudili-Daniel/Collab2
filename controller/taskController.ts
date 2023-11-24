104910c96205171102634c08fb164fd877ac70c2
dennyblack11 <dennisozoemena3@gmail.com>
2023-11-24 14:03:30 +0100

    ControllerUpdated

--- controller/taskController.ts
+++ controller/taskController.ts
@@ -80,5 +80,25 @@ export const updateTask = async(req: Request, res:Response)=>{
 };
 
 
+export const deleteTask = async(req: Request, res:Response)=>{
+    try {
+
+        const { taskID} = req.params
+
+        const user = await TaskModel.findByIdAndDelete({taskID})
+
+        return res.status(200).json({
+            message:"success in deleting one task",
+            data: user
+        })
+    } catch (error) {
+        return res.status(404).json({
+            message:"error"
+        })
+    }
+    
+};
+
+
 
 
