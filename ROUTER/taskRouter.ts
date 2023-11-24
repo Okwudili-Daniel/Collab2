883422caa96b1ec488ab67e001341d64b162f150
JoanJeca <joannajeca936@gmail.com>
2023-11-24 13:36:36 +0100

    Router

--- ROUTER/taskRouter.ts
+++ ROUTER/taskRouter.ts
@@ -1,6 +1,9 @@
 import {Router} from 'express'
+import { createTask, viewTask } from '../controller/taskController'
 
 const router:Router = Router()
 
-router.route("/create-task")
+router.route("/create-task").post(createTask)
+router.route("/view-task").get(viewTask)
+
 export default router
